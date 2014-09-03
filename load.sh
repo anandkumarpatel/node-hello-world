2513c47512b1508af4d0431196bfa07c5a3dee75
anandkumarpatel <anand@runnable.com>
2014-09-03 15:54:25 -0700

    Update load.sh

--- load.sh
+++ load.sh
@@ -2,7 +2,8 @@
 cd /runnable/node-hello-world
 T="$(date +%s%N)"
 sudo docker build --no-cache . > /dev/null 2>&1
+E=$?
 T="$(($(date +%s%N)-T))"
 M="$((T/1000000))"
-echo "TIME_IN_MS: " $M >> data
+echo "TIME_IN_MS " $M " " $E >> data
 
