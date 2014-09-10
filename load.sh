7189716ecbf62ccd3e88c35734169921f8c101de
anandkumarpatel <anand@runnable.com>
2014-09-10 13:08:55 -0700

    Update load.sh

--- load.sh
+++ load.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /runnable/node-hello-world
 T="$(date +%s%N)"
-sudo docker build --no-cache . > /dev/null 2>&1
+sudo docker build --no-cache . >> /runnable/node-hello-world/logs 2>&1
 E=$?
 T="$(($(date +%s%N)-T))"
 M="$((T/1000000))"
