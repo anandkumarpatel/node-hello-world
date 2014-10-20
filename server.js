ec6bc65f96c0ef7b7f4d730a49447dc61bbc4b00
anandkumarpatel <anand@runnable.com>
2014-10-20 14:37:34 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Seth\n');
+  res.end('Hello DJ FaZe\n');
 }).listen(52738);
 console.log('Server running at http://127.0.0.1:52738/');
