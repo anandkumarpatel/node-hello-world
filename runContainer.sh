0fb5273011abc27efc49770c0d6f056c7e545608
anandkumarpatel <anand@runnable.com>
2015-01-26 14:29:41 -0800

    Update runContainer.sh

--- runContainer.sh
+++ runContainer.sh
@@ -5,4 +5,5 @@ sudo docker build --no-cache . >> /dev/null 2>&1
 E=$?
 T="$(($(date +%s%N)-T))"
 M="$((T/1000000))"
-echo "BUILD_TIME_MS " $M " EXIT_CODE " $E >> $1
+# csv format BUILD_TIME_MS, EXIT_CODE
+echo  "$M,$E" >> $1
