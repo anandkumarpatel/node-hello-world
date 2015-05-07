56b9157686b50977d25749a64c954d9a7bacb3ca
anandkumarpatel <anand@runnable.com>
2015-05-06 17:47:42 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var port = process.env.PORT || 52738;
 http.createServer(function (req, res) {
+  console.log(req.headers);
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello DJ FaZe\n');
 }).listen(port);
