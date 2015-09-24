8db9d57979ffc8f1be642f0562f2b57594e6e5a8
anandkumarpatel <anand@runnable.com>
2015-09-23 17:01:34 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http');
 var port = process.env.PORT || 80;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello DJ F1aZe !!!!1!!!\n');
+  res.end('Hello DJ FaZe\n');
 }).listen(port);
 console.log('Server running at http://127.0.0.1:'+port);
